82554ec3e9c06023a08863c72872bf32ea5ebe50
dchen278 <dchen30@stuy.edu>
2022-06-03 11:09:32 -0400

    v7 stuff and some bug fixes

---
+++ v7/Woo.java
@@ -0,0 +1,77 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import java.util.Stack;
+
+import components.Application;
+import core.Calculator;
+import core.Taskbar;
+
+public class Woo extends JFrame {
+    
+    private Stack<JPanel> panels;
+    
+    private Woo() {
+        super();
+
+        Dimension size = new Dimension(800, 600);
+
+        this.setSize(size);
+        this.setTitle("Windows95");
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        this.setIconImage(new ImageIcon("./assets/windows95.png").getImage());
+
+        JLayeredPane mainWindow = new JLayeredPane();
+
+        JPanel homescreen = new JPanel();
+        // default fullscreen yet allows for resizing
+        homescreen.setSize(new Dimension(
+            (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * 100),
+            (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 70)
+        ));
+        homescreen.setBackground(new Color(0x55aaaa));
+        homescreen.setLayout(new BorderLayout());
+
+        JPanel desktop = new JPanel();
+        desktop.setLayout(new FlowLayout(FlowLayout.LEADING));
+        desktop.setOpaque(false);
+
+        Taskbar taskbar = new Taskbar();
+
+        homescreen.add(desktop, BorderLayout.WEST);
+        homescreen.add(taskbar, BorderLayout.SOUTH);
+
+        JLayeredPane appsPane = new JLayeredPane();
+        appsPane.setSize(size);
+        appsPane.setOpaque(false);
+
+        mainWindow.add(homescreen, Integer.valueOf(0));
+        mainWindow.add(appsPane, Integer.valueOf(1));
+
+        this.add(mainWindow, BorderLayout.CENTER);
+
+        new Application("File Explorer", "./assets/explorer.png", desktop, taskbar, appsPane);
+        new Application("Paint", "./assets/paint.png", desktop, taskbar, appsPane);
+        new Application("Notepad", "./assets/notepad.png", desktop, taskbar, appsPane);
+        new Calculator(desktop, taskbar, appsPane);
+        new Application("TicTacToe", "./assets/game.png", desktop, taskbar, appsPane);
+        new Application("test", "./assets/windows.png", desktop, taskbar, appsPane);
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Woo();
+    }
+}
