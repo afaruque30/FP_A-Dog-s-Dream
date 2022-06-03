95bd3e7640a7273e5d8115f16e18b4b4b55d184b
Abduller Maraco <afaruque@stuy.edu>
2022-06-02 23:43:34 -0400

    massive changes, read devlog

---
+++ v6/core/Taskbar.java
@@ -0,0 +1,38 @@
+package core;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Taskbar extends JPanel implements ActionListener {
+    private JButton startButton;
+
+    public Taskbar() {
+        super();
+
+        this.setBackground(new Color(0xc3c7cb));
+        this.setPreferredSize(new Dimension(100, 50));
+        this.setLayout(new FlowLayout(FlowLayout.LEADING));
+        this.setBorder(new EmptyBorder(2, 2, 2, 2));
+
+        this.startButton = new JButton("Start");
+        this.startButton.setBackground(new Color(0xc3c7cb));
+        this.startButton.setIcon(new ImageIcon(new ImageIcon("./assets/windows.png").getImage()
+                .getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+
+        this.add(this.startButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // document why this method is empty
+    }
+}
---
+++ v6/core/TicTacToe.java
@@ -0,0 +1,14 @@
+package core;
+
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+import components.Application;
+
+public class TicTacToe extends Application {
+
+    public TicTacToe(String appName, String iconPath, JPanel desktop, Taskbar taskbar, JLayeredPane appsPane) {
+        super(appName, iconPath, desktop, taskbar, appsPane);
+        //TODO Auto-generated constructor stub
+    }
+}
