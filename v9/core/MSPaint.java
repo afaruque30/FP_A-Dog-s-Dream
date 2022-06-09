b4ec6c66432065257ab167ad16cc5fbca5e12916
djung30 <djung30@stuy.edu>
2022-06-09 00:20:51 -0400

    progress on buttons and layout for mspaint

--- v9/core/MSPaint.java
+++ v9/core/MSPaint.java
@@ -3,6 +3,7 @@ package core;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Color;
 import java.util.Arrays;
 
 import javax.swing.JButton;
@@ -17,15 +18,18 @@ import components.Application;
 public class MSPaint extends Application {
     String currentColor;
     double strokeWidth;
-    private final int blue = 255;
-    private double history;
-    private String prevOp;
+
+    protected static final Color yellow = new Color(255,255,0);
+    protected static final Color green = new Color(0,128,0);
+    protected static final Color cyan = new Color(0,255,255);
+    protected static final Color blue = new Color(0,0,255);
+    protected static final Color magenta = new Color(255,0,255);
+    protected static final Color pink = new Color(255,20,147);
+    protected static final Color red = new Color(255,0,0);
 
     public MSPaint(JPanel desktop, Taskbar taskbar, JLayeredPane appsPane) {
         super("MS Paint", "./assets/paint.png", desktop, taskbar, appsPane);
         //TODO Auto-generated constructor stub
-        this.history = 0;
-        this.prevOp = "";
     }
 
     @Override
@@ -35,108 +39,31 @@ public class MSPaint extends Application {
         this.window.setSize(1200, 800);
 
         this.content.setLayout(new BorderLayout());
-        this.content.setBorder(new EmptyBorder(4, 4, 10, 4));
+        this.content.setBorder(new EmptyBorder(4, 4, 4, 4));
         this.content.setBackground(Application.GREY);
 
-        JLabel display = new JLabel("");
-        display.setHorizontalAlignment(SwingConstants.RIGHT);
-        display.setPreferredSize(new Dimension(100, 30));
+        JPanel display = new JPanel();
+        display.setPreferredSize(new Dimension(1100, 692));
         display.setBorder(new EmptyBorder(4, 4, 4, 4));
 
-        JPanel keyPad = new JPanel(new GridLayout(5, 4, 4, 6));
+        JPanel keyPad = new JPanel(new GridLayout(2, 4, 4, 4));
         keyPad.setOpaque(false);
-        for (String numString : new String[] {
-                "C", "√", "x²", "*",
-                "1", "2", "3", "/",
-                "4", "5", "6", "+",
-                "7", "8", "9", "_",
-                "-", "0", ".", "="
+        Color[] colorArray = new Color[]{yellow, green, cyan, blue, magenta, pink, red, Color.white};
+        int i = 0;
+        for (String buttonColor : new String[] {
+                "yellow", "green", "cyan", "blue", 
+                "magenta", "pink", "red", "eraser"
         }) {
-            JButton key = new JButton(numString);
-            key.setBackground(Application.GREY);
-            key.addActionListener(e -> {
-                Double displayVal;
-                if ("".equals(display.getText())) {
-                    displayVal = 0.0;
-                } else {
-                    displayVal = Double.parseDouble(display.getText());
-                }
-                String text = ((JButton) e.getSource()).getText();
-                String[] ops = { "*", "/", "+", "_", "=" };
-                String[] specs = { "C", "√", "x²" };
-                String[] nums = {
-                        "1", "2", "3",
-                        "4", "5", "6",
-                        "7", "8", "9",
-                        "-", "0", "."
-                };
-                if (Arrays.asList(ops).contains(text)) {
-                    switch (this.prevOp) {
-                        case "*":
-                            this.history *= displayVal;
-                            break;
-                        case "/":
-                            if (displayVal != 0) {
-                                this.history /= displayVal;
-                            }
-                            break;
-                        case "+":
-                            this.history += displayVal;
-                            break;
-                        case "_":
-                            this.history -= displayVal;
-                            break;
-                        default:
-                            this.history = displayVal;
-                    }
-                    this.prevOp = text;
-                    if (!"=".equals(text)) {
-                        display.setText("");
-                    } else {
-                        display.setText(String.format("%.0f", this.history));
-                    }
-                } else if (Arrays.asList(specs).contains(text)) {
-                    switch (text) {
-                        case "C":
-                            display.setText("");
-                            this.history = 0;
-                            this.prevOp = "";
-                            break;
-                        case "√":
-                            display.setText(String.valueOf(Math.sqrt(displayVal)));
-                            break;
-                        case "x²":
-                            display.setText(String.valueOf(Math.pow(displayVal, 2)));
-                            break;
-                        default:
-                    }
-                } else if (Arrays.asList(nums).contains(text)) {
-                    switch (text) {
-                        case "-":
-                            if (!"".equals(display.getText())) {
-                                if (display.getText().charAt(0) == '-') {
-                                    display.setText(display.getText().substring(1));
-                                } else {
-                                    display.setText('-' + display.getText());
-                                }
-                            }
-                            break;
-                        case ".":
-                            if (!"".equals(display.getText())
-                                    && !display.getText().contains(".")) {
-                                display.setText(display.getText() + '.');
-                            }
-                            break;
-                        default:
-                            display.setText(display.getText() + text);
-                    }
-                }
-            });
+            JButton key = new JButton(buttonColor);
+            key.setOpaque(true);
+            key.setBackground(colorArray[i]);
             keyPad.add(key);
-        }
+            i++;
+        
 
         this.content.add(display, BorderLayout.NORTH);
         this.content.add(keyPad, BorderLayout.SOUTH);
+        }
     }
 
 
